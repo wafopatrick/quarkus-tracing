5a6dcf1d02cca2cd72d48727d03e95cd686870d7
patrick wafo <wafo.digital@gmail.com>
2025-08-19 11:24:53 +0200

    feat: add helm repo commands to deploy-infra.sh

--- scripts/deploy-infra.sh
+++ scripts/deploy-infra.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
+helm repo add grafana https://grafana.github.io/helm-charts
+helm repo add jaegertracing https://jaegertracing.github.io/helm-charts
+helm repo add bitnami https://charts.bitnami.com/bitnami
+helm repo update
+
 # Deploy infrastructure using Helm
 helm upgrade --install grafana grafana/grafana -f helm/infra-charts/grafana-values.yaml --namespace observability --create-namespace
 helm upgrade --install loki grafana/loki-stack -f helm/infra-charts/loki-values.yaml --namespace logging --create-namespace
